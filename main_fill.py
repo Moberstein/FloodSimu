01da5ee03b55ae50e4ad8b51fdbe4633ddd6a9fb
grimUnicorn <grimmingerapps@gmail.com>
2019-05-12 11:14:24 +0200

    fancy design improvement

--- main_fill.py
+++ main_fill.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 
 #%%
 building = geopandas.read_file("./gis_osm_buildings.shp");
-xyzdata = pandas.read_csv("./smallxyz.csv", header=None);
+xyzdata = pandas.read_csv("./smallxyz2.csv", header=None);
 #print(building.head())
 #print(xyzdata.head())
 
@@ -42,10 +42,8 @@ while drawpoints.empty:
     drawpoints = points.loc[(points['height']<threshold)]
     threshold = threshold + 1
 
-drawpoints.plot(ax=ax, color='blue', alpha=0.2)
+drawpoints.plot(ax=ax, color='blue', alpha=0.2, markersize=6)
 
-drawList = [drawpoints]
-drawList_New = []
 while threshold < 200:  
     
     threshold = threshold + 1
@@ -54,25 +52,6 @@ while threshold < 200:
     plt.pause(1)
 
     drawpoints_new = points.loc[(points['height']<threshold) & (points['height'] >= threshold - 1)]
-    drawpoints_new.plot(ax=ax, column="height", alpha=0.2)
-    #for drawElement in drawList:
-    #    for point in drawElement.itertuples():
-    #        x = point[1]
-    #        y = point[2]
-    #near_points = drawpoints_new.loc[(drawpoints_new['x'] <= x+1 ) & (drawpoints_new['x'] >= x-1) & (drawpoints_new['y'] <= y+1) & (drawpoints_new['y'] >= y-1)]
-    #near_points.plot(ax=ax, color='red', alpha=0.2)
-    #drawList_New.append(near_points)
-
-    #print("drawlist" ,len(drawList))
-    #print("drawlist new",len(drawList_New))
-    #drawList = drawList_New
-    #drawList_New = []
-    #print("drawlist" ,len(drawList))
-    #print("drawlist new",len(drawList_New))
+    drawpoints_new.plot(ax=ax, column="height", alpha=0.15, markersize=6)
 
 plt.show()
-#print("test");
-#inFile = File('../sampledata/lidar.las', mode='r');
-
-
-#%%
